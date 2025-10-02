2b77fc424aa4dbeebad9748da0b5998d88aab10d
talha24603 <your_email_used_in_talha083>
2025-10-02 21:50:39 +0500

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,9 @@ import { NextResponse } from "next/server";
 export function middleware(req: Request) {
   const res = NextResponse.next();
 
-  res.headers.set("Access-Control-Allow-Origin", "https://cui-internship-system.vercel.app");
+  res.headers.set("Access-Control-Allow-Origin", "*");
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.headers.set("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
     return new NextResponse(null, { status: 204, headers: res.headers });
